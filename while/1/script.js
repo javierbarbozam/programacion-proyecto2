28a350b6af2669c8da247640114f5aecf6c55403
javierbarbozam <barboza.javier497@gmail.com>
2022-07-14 14:06:56 -0600

    First While Exercise

---
+++ while/1/script.js
@@ -0,0 +1,22 @@
+let prompt1 = prompt ("Inserte un número")
+let prompt2 = prompt ("Inserte un segundo número")
+let n1 = parseInt(prompt1)
+let n2 = parseInt(prompt2)
+
+
+while(n1 > n2){
+	let sumatoria = n2 + 1
+	document.write(sumatoria)
+}
+
+
+
+/*
+
+contador = 0
+while(contador <=10){
+	console.log("Ejecución" , contador)
+	contador ++;
+}
+
+*/
