7d509dd404c89e0012ccceadfe1ed9bcd7c1f249
javierbarbozam <barboza.javier497@gmail.com>
2022-07-19 01:12:51 -0600

    Second For Finished

--- for/2/script.js
+++ for/2/script.js
@@ -1,47 +1,11 @@
-let numero = Number(prompt("Digite un número del 1 al 10"))
-let resultado=0
+let lista = [12,75,150,180,145,525,50]
 
-for (let i = 1; i <=10; i++){
-	resultado= i*numero
-	document.write(resultado)
-	document.write("<br>")
+for (let i = 0; i < lista.length; i++){
+	if(lista[i] % 5 == 0 && lista[i] < 150){
+		document.write(lista[i])
+		document.write("<br>")
+	}
+	if(lista[i] > 500){
+		break;
+	}
 }
-
-/*
-let listadenumero = [];
-for (let i = 0; i <= 10; i++) {
-  listadenumero.push(i);
-}
-for (let i = 0; i < listadenumero.length; i++) {
-  if (i % 2 == 0) {
-    document.writeln(`numero: ${listadenumero[i]}`);
-    document.write("</br>");
-  }
-}
-
-----------------------------------
-
-let numero = prompt("Ingrese un número de 6 digitos");
-let contador = 0;
-let ultimacifra = numero % 10;
-
-for (let i = 0; i <= parseInt(numero); i++) {
-  if (i % 10 == numero % 10) {
-    contador = contador + 1;
-  }
-}
-document.write(contador);
-
------------------------------------
-
-let finDeCiclo = 10;
-let sumatoria = 0;
-
-for (let i = 0; i < finDeCiclo; i++) {
-	 if (i % 2 == 0){
-			 sumatoria = sumatoria + i
-	 }
-}
-document.write(sumatoria)
-*/
-
